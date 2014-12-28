9030fa3c85d946317a68caa08a84632aea3e3825
Kelvin Leong <kelvin.leong@berkeley.edu>
2014-12-28 00:44:40 -0800

    redid UIDatePicker with just day of week and date

--- EventCount/EditDateViewController.h
+++ EventCount/EditDateViewController.h
@@ -9,14 +9,14 @@
 #import <UIKit/UIKit.h>
 
 #import "Mark.h"
+#import "AADatePicker.h"
 
 @interface EditDateViewController : UIViewController
 
 @property (nonatomic) Mark *currentMark;
 @property (nonatomic) int selectedIndex;
 
-@property BOOL needToResort;
-
-@property (weak, nonatomic) IBOutlet UIDatePicker *datePicker;
+//@property BOOL needToResort;
+@property AADatePicker *datePicker;
 
 @end
--- EventCount/EditDateViewController.m
+++ EventCount/EditDateViewController.m
@@ -7,64 +7,69 @@
 //
 
 #import "EditDateViewController.h"
+#import "AADatePicker.h"
+
+@interface EditDateViewController () <AADatePickerDelegate>
+
+@property (strong, nonatomic) UILabel *dateLabel;
+
+@end
 
 @implementation EditDateViewController
 
-@synthesize selectedIndex, currentMark, datePicker, needToResort;
+@synthesize selectedIndex, currentMark, datePicker;
 
 - (void)viewDidLoad {
-    NSDate *defaultDate = currentMark.datesUsed[selectedIndex];
-    datePicker.date = defaultDate;
+    datePicker = [[AADatePicker alloc] initWithFrame:CGRectMake(0, 130, 320, 264) maxDate:[NSDate dateWithTimeIntervalSinceNow:7*24*60*60] minDate:[NSDate date] showValidDatesOnly:NO];
     
-    [self configureDates];
+    datePicker.delegate = self;
+    [self.view addSubview:datePicker];
+    
+    NSDate *defaultDate = currentMark.datesUsed[selectedIndex];
+    [datePicker showDateOnPicker:defaultDate];
 }
 
-- (void)configureDates {
-    needToResort = false; // default
-    
-    datePicker.maximumDate = [NSDate date];
-    
-    // first index
-    if (selectedIndex == 0) {
-        
-        // there is more than one date, so use the next date as max
-        if ([currentMark.datesUsed count] > 1)
-            datePicker.maximumDate = currentMark.datesUsed[selectedIndex + 1];
-        
-        // if only one, it will return anyway
-        return;
-    }
-    
-    // last index should NOT have minimum, in case user creates new date and wants it to be moved somewhere
-    if (selectedIndex == [currentMark.datesUsed count] - 1) {
-        needToResort = true;
-        return;
-    }
-    
-    // default with a before and after
-    datePicker.minimumDate = currentMark.datesUsed[selectedIndex - 1];
-    datePicker.maximumDate = currentMark.datesUsed[selectedIndex + 1];
-}
+//- (void)configureDates {
+//    needToResort = false; // default
+//    
+//    // first index
+//    if (selectedIndex == 0) {
+//        
+//        // there is more than one date, so use the next date as max
+//        if ([currentMark.datesUsed count] > 1)
+//            datePicker.maxDate = currentMark.datesUsed[selectedIndex + 1];
+//        
+//        // if only one, it will return anyway
+//        return;
+//    }
+//    
+//    // last index should NOT have minimum, in case user creates new date and wants it to be moved somewhere
+//    if (selectedIndex == [currentMark.datesUsed count] - 1) {
+//        needToResort = true;
+//        return;
+//    }
+//    
+//    // default with a before and after
+////    datePicker.minDate = currentMark.datesUsed[selectedIndex - 1];
+////    datePicker.maxDate = currentMark.datesUsed[selectedIndex + 1];
+//}
 
 - (IBAction)save:(id)sender {
     // change the date of the index
-    currentMark.datesUsed[selectedIndex] = datePicker.date; // will be erased anyway in if statement
+//    currentMark.datesUsed[selectedIndex] = date; // will be erased anyway in if statement
     
-    if (needToResort) {
-        NSDate *resort = datePicker.date;
+    NSDate *resort = datePicker.date;
         
-        [currentMark.datesUsed removeObjectAtIndex:[currentMark.datesUsed count] - 1]; // remove last, which is what was changed
-        selectedIndex--; // to account for the removal
+    [currentMark.datesUsed removeObjectAtIndex:[currentMark.datesUsed count] - 1]; // remove last, which is what was changed
+    selectedIndex--; // to account for the removal
         
-        // find spot for the changed date
-        while (selectedIndex >= 0 && ([resort earlierDate:currentMark.datesUsed[selectedIndex]] == resort))
-            selectedIndex--;
+    // find spot for the changed date
+    while (selectedIndex >= 0 && ([resort earlierDate:currentMark.datesUsed[selectedIndex]] == resort))
+        selectedIndex--;
         
-        selectedIndex++; // index of where the object goes (loop goes 1 further)
+    selectedIndex++; // index of where the object goes (loop goes 1 further)
         
-        [currentMark.datesUsed insertObject:resort atIndex:selectedIndex];
-        
-    }
+    [currentMark.datesUsed insertObject:resort atIndex:selectedIndex];
     
     [[[self.navigationController viewControllers] objectAtIndex:1] viewDidLoad];
     
